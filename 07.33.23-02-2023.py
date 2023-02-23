d78da931ff68bcd75ec7593d4226fc5802d0b00f
1485849 <1485849@senati.pe>
2023-02-23 07:58:41 -0500

    promedio 2 agregado

--- 07.33.23-02-2023.py
+++ 07.33.23-02-2023.py
@@ -7,3 +7,15 @@ print("el promedio es: ",promedio)
 # primera nota: 16
 # segunda nota: 18
 # tercera nota : 17
+
+# segundo promedio
+
+d=int(input("ingrese la nota 1: "))
+e=int(input("ingese la nota 2: "))
+f=int(input("ingrese la nota 3: "))
+promedio2=((d+e+f)/3)
+print("el segundo promedio es: ",promedio2)
+
+# 1° nota es.20
+# 2° nota es: 16
+# 3° nota es:18
