f29c4c93fa3a88033eba3d4fd0294b801e0691dc
1485849 <1485849@senati.pe>
2023-02-23 07:46:07 -0500

    promedio en python

---
+++ 07.33.23-02-2023.py
@@ -0,0 +1,9 @@
+a=int(input("ingrese la nota 1: "))
+b=int(input("ingrese la nota 2: "))
+c=int(input("ingrese la nota 3: "))
+promedio=((a+b+c)/3)
+print("el promedio es: ",promedio)
+
+# primera nota: 16
+# segunda nota: 18
+# tercera nota : 17
