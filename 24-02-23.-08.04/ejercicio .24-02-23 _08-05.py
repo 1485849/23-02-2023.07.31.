b3b2ac5f32a42964caa0454980ed675bb41086c2
1485849 <1485849@senati.pe>
2023-02-24 10:16:48 -0500

    metodo agregado

--- 24-02-23.-08.04/ejercicio .24-02-23 _08-05.py
+++ 24-02-23.-08.04/ejercicio .24-02-23 _08-05.py
@@ -6,9 +6,26 @@
 
 a=int(0)
 
-
 for  x in range(0,a+1):
+    
    print("0   " "0  " "la suma es: ",0+(0-0))
+   print("1   " "0  " "la suma es: ",1+(1-0))
+   print("2   " "1  " "la suma es: ",2+(2-1))
+   print("3   " "2  " "la suma es: ",3+(3-1))
+   print("4   " "3  " "la suma es: ",4+(4-1))
+   print("5   " "4  " "la suma es: ",5+(5-1))
+   print("6   " "5  " "la suma es: ",6+(6-1))
+   print("7   " "6  " "la suma es: ",7+(7-1))
+   print("8   " "7  " "la suma es: ",8+(8-1))
+   print("9   " "8  " "la suma es: ",9+(9-1))
+   print("10  " "9  " "la suma es: ",10+(10-1))
    
+   print(0+(0-0))
+   print(1+(1-1))
+   print(2+(2-1))
+   print(3+(3-1))
+   print(4+(4-1))
+   
+
     
 
