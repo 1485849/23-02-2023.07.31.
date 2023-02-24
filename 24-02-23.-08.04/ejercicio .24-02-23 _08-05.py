38512007ffc66dc76d792d7f7bc57d92cd490567
1485849 <1485849@senati.pe>
2023-02-24 11:28:16 -0500

    actualizado

--- 24-02-23.-08.04/ejercicio .24-02-23 _08-05.py
+++ 24-02-23.-08.04/ejercicio .24-02-23 _08-05.py
@@ -9,6 +9,14 @@ a=int(0)
 for  x in range(0,a):
     print(x+(x-1))
     
+print(18+(18-1))  
+print(0+(0-0))
+print(1+(1-1))
+print(2+(2-1))
+print(3+(3-1))
+print(4+(4-1))    
+
+
 print("0   " "0  " "la suma es: ",0+(0-0))
 print("1   " "0  " "la suma es: ",1+(1-0))
 print("2   " "1  " "la suma es: ",2+(2-1))
@@ -20,14 +28,6 @@ print("7   " "6  " "la suma es: ",7+(7-1))
 print("8   " "7  " "la suma es: ",8+(8-1))
 print("9   " "8  " "la suma es: ",9+(9-1))
 print("10  " "9  " "la suma es: ",10+(10-1))
-
-   
-print(0+(0-0))
-print(1+(1-1))
-print(2+(2-1))
-print(3+(3-1))
-print(4+(4-1))
-   
 
     
 
