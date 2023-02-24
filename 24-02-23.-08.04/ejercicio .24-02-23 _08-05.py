90fa4bfab66bcb7243fdcc42b090d3506cf99547
1485849 <1485849@senati.pe>
2023-02-24 09:50:45 -0500

    suma de numero actual y anterior

---
+++ 24-02-23.-08.04/ejercicio .24-02-23 _08-05.py
@@ -0,0 +1,14 @@
+# desarrolle un algoritmo e la que muestre la suma 
+# del numero actual y el anterior 
+
+# (10)
+
+
+a=int(0)
+
+
+for  x in range(0,a+1):
+   print("0   " "0  " "la suma es: ",0+(0-0))
+   
+    
+
