3416a16dd8b9fb356371315bc44448022e2e2e4f
unknown <1485849>
2023-02-25 08:54:31 -0500

    agregado detalles

---
+++ 25-02-23.-08-02/divisores de 5 .py
@@ -0,0 +1,9 @@
+
+# Realizar un algoritmo y que muestre los numeros divisibles entre 5 de una lista de numeros 
+
+numeros = {10, 20, 33, 46, 55}
+
+divisible = int(input("ingrese un numero de la lista: "))
+if((divisible % 5 == 10)):
+    print("en numero 10 es divisible entre 5")
+    
