2356830063c8ffcbb312c10f07dc3e00f47cf87b
unknown <1485849>
2023-02-25 09:12:20 -0500

    primer codigo

--- 25-02-23_08-50/secuencia de numeros.py
+++ 25-02-23_08-50/secuencia de numeros.py
@@ -8,4 +8,8 @@
 # 5 5 5 5 5
 
 n = int(input("introduce los numeros: "))
-for i in range (1,n+1)
+for a in range (1, n+1, 2):
+    for b in range(a, 0, -2):
+        print(b, end=" ")
+        print(" ")
+             
